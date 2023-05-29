bf20b93ed8281be5d732b04d4724c53b0f093e88
Andrey <a@a.ru>
2023-05-29 21:58:20 +0200

    Выполнено: 1. Кастомные страницы для ошибок. 2. Отправка электронной почты

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.page_server_error'
+
 urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('pages/', include('pages.urls', namespace='pages')),
