2c9ddfeaab6030a74a80ea9f418cf655f1a7480a
Иванов Андрей <123881893+Mist3s@users.noreply.github.com>
2024-01-18 10:48:04 +0200

    Delete blogicum/blog/serializers.py

--- blogicum/blog/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('text', 'author', 'pub_date')
-        model = Post
